5864b691f50138e46ef2800c86e1716a608a2c10
Daylen Smith <daylencsmith@gmail.com>
2025-09-13 22:45:29 -0700

    some changes

--- js/writer.js
+++ js/writer.js
@@ -16,17 +16,19 @@ class Content {
         this.writerContainer = new WriterNoteContainer(this.writerElement);
         body.appendChild(this.writerElement)
 
+        this.writerContainer.addNotes();
+
         this.addButtonCntainer = document.createElement("div")
         body.append(this.addButtonCntainer)
         this.addButton = new Button(Messages.ADD, [], () => {
             this.writerContainer.addNote("", -1)
+            StorageAccess.saveNotes(this.writerContainer.getAllTexts()) 
         })
         this.addButton.render(this.addButtonCntainer)
 
         const backButton = new RedirectButton(Messages.BACK, [], Pages.INDEX);
         backButton.render(body);
 
-        this.update()
         this.startUpdatingTimestamp();
     }
 
@@ -36,9 +38,6 @@ class Content {
 
     update() {
         this.updatedAt.innerHTML = Messages.UPDATED_AT(this.getFormattedTime());
-        StorageAccess.saveNotes(this.writerContainer.getAllTexts())
-        this.writerContainer.clear()
-        this.writerContainer.addNotes()
     }
 
     startUpdatingTimestamp() {
